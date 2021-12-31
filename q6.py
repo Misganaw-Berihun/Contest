c7a2de4afea9c51e60af84af0362e6dfbb890170
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2021-12-31 09:37:36 +0300

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,3 +1,9 @@
+#Question: Given an integer array, move all 0's to the end maintaining the order of the other elements.
+#                        Example 1:
+#                        Input: nums = [0,1,0,3,12]
+#                        Output: [1,3,12,0,0]
+
+#Time: O(n), Memory: O(n)
 def main():
     print("Enter the elements of the list (separated by space): ")
     arr = list(map(int,input().split()))
